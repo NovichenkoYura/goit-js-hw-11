00fcf09e3ad759659d807711b48d49fd1960d685
NovichenkoYura <novichenko.y@gmail.com>
2022-06-16 16:33:01 +0300

    loadmore doesnt't work

--- src/index.js
+++ src/index.js
@@ -6,32 +6,26 @@ import Notiflix from 'notiflix';
 
 const API_KEY = '28025746-c4df93d18fee554bb72ff63c8';
 const BASE_URL = 'https://pixabay.com/api/';
+let resultLength;
+let totalHits;
+let currentPage;
+
+
 
 const searchForm = document.querySelector('.search-form');
-const galleryList = document.querySelector('.gallery')
+const galleryList = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more')
+loadMoreBtn.style.display = 'none';
 
-searchForm.addEventListener('submit', onFormSubmitSearchPhotos)
+searchForm.addEventListener('submit', onFormSubmitSearchPhotos);
+loadMoreBtn.addEventListener('click', onLoadMore)
 
-function onFormSubmitSearchPhotos() {
-  getPhotos;
-  renderPhotos;    
-}
-
-
-async function getPhotos(e) {
+async function onFormSubmitSearchPhotos(e) {
   e.preventDefault()
-    const result = await axios.get(`${BASE_URL}`, {
-    params: {
-             key: API_KEY,
-            q: e.target[0].value,
-             image_type: "photo",
-             orientation: "horizontal",
-             safesearch: true,        
-      }
-     })
-  return result
+  const keyWord = e.target[0].value;
+  const photos = await getPhotos(keyWord);
+  renderPhotos(photos);
 }
-
 
 
 function renderPhotos(photos) {
@@ -59,5 +53,66 @@ function renderPhotos(photos) {
 </div>`
   })
     .join('');
-  galleryList.insertAdjacentHTML('beforeend', renderPhotos);
+  galleryList.insertAdjacentHTML('beforeend', markup);
+  loadMoreBtn.style.display = 'block';
+}
+
+async function getPhotos(word) {
+
+  try {
+    const result = await axios.get(`${BASE_URL}`, {
+      params: {
+        key: API_KEY,
+        q: word,
+        image_type: "photo",
+        orientation: "horizontal",
+        safesearch: true,
+        per_page: 40,
+      }
+    });
+
+    currentPage = 1;
+    resultLength += result.data.hits.length;
+    totalHits = result.data.totalHits;
+    
+    
+    if (totalHits < 1) {
+      // loadMoreBtn.classList.add('ishidden')
+      return errorSearchPhotos()
+    }
+
+    return result.data.hits;    
+ 
+  } catch {
+    console.log(error);
+   } 
+  
+}
+
+
+async function onLoadMore() {
+  try {
+    currentPage += 1;
+   
+    const imageAdd = await getPhotos(keyWord);   
+    await renderPhotos(imageAdd);
+    
+    resultLength += result.data.hits.length;
+    totalHits = result.data.totalHits;
+    if (resultLength >= totalHits) {
+      return enoughSearchPhotos()
+    }
+  }
+  catch {
+    console.log(error);
+
+  }
+}
+
+function errorSearchPhotos() {
+    Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+}
+
+function enoughSearchPhotos() {
+  Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
 }
